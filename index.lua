11c05a4e5b93ff8e845accb07035e4b314485209
doujiang24 <doujiang24@gmail.com>
2014-03-05 18:15:59 +0800

    optmize, system

--- index.lua
+++ index.lua
@@ -9,11 +9,4 @@ local router = require "core.router"
 local rt = router:new()
 dp.router = rt
 
-local ctr, func, args = rt:route()
-
-if not ctr then
-    ngx.exit(404)
-end
-
---assert(pcall(ctr[func], unpack(args)))
-ctr[func](unpack(args))
+return rt:run()
