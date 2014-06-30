4e6f141e32e86fa2ddadcaa14453364b48021c30
doujiang24 <doujiang24@gmail.com>
2014-06-30 18:22:43 +0800

    root_path, appname from dpconfig

---
+++ dpconfig.lua
@@ -0,0 +1,18 @@
+-- Copyright (C) Dejiang Zhu (doujiang24)
+
+
+local _M = { }
+
+
+_M.root_path = "/home/dou/work/git/doujiang/durap/"
+
+_M.applications = {
+    ["dou.com"] = "blog",
+    ["do.com"]  = "demo1",
+
+    default     = "demo1",
+}
+
+
+return _M
+
