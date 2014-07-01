1df54ab9c2080f1211fb472c55d7ed402e889350
doujiang24 <doujiang24@gmail.com>
2014-07-01 12:06:01 +0800

    add background job demo in init_worker

---
+++ worker.lua
@@ -0,0 +1,11 @@
+-- Copyright (C) Dejiang Zhu (doujiang24)
+
+
+local back_run  = require "system.library.timer" .run
+
+--[[
+--  we can start some background jobs like this:
+--  back_run(5, "mobileservice", "vote", "statistics")
+--
+--  this means will run function 'statistics' in contoller 'welcome' in application 'demo1' every 5 seconds
+--]]
